11c3f86b7fe2d18159d393ae144d0a3dfac7de48
Cosme Valera <cosmevalerareales@gmail.com>
2026-09-25 23:52:19 +0200

    Add saludo method with params

--- frontend/angular/angular-java-exercice/backend/src/main/java/com/cosme/backend/HelloController.java
+++ frontend/angular/angular-java-exercice/backend/src/main/java/com/cosme/backend/HelloController.java
@@ -1,13 +1,21 @@
 package com.cosme.backend;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController 
+@RequestMapping("/api")
 public class HelloController {
   
-  @GetMapping("/api/hello")
+  @GetMapping("/hello")
   public String hello() {
     return "Hello from Spring!";
+  }
+
+  @GetMapping("/saludo/{name}/{edad}")
+  public String saludo(@PathVariable String name, @PathVariable int edad) {
+    return "Hello from spring, " + name + " your age is " + edad;
   }
 }
